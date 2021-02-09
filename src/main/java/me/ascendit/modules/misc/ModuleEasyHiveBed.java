4f5988dac932be0c9723069c6a9cfaf4b5893697
NotNanook <gogabrys@gmail.com>
2021-02-09 09:29:18 +0100

    fixed hivebed

--- src/main/java/me/ascendit/modules/misc/ModuleEasyHiveBed.java
+++ src/main/java/me/ascendit/modules/misc/ModuleEasyHiveBed.java
@@ -12,11 +12,13 @@ import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.Vec3;
+import net.minecraft.util.Vec3i;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
 public class ModuleEasyHiveBed extends Module{
 	
 	protected int radius = 3;
+	protected int counter; // makes sure that it only places blocks every 5 ticks (otherwise it wont work)
 	
 	public ModuleEasyHiveBed()
 	{
@@ -28,6 +30,7 @@ public class ModuleEasyHiveBed extends Module{
 	@Override
 	public void onEnable() 
 	{
+		counter = 0;
 		mc.thePlayer.addChatComponentMessage(new ChatComponentText("[" + EnumChatFormatting.GREEN + "EasyHiveBed" + EnumChatFormatting.WHITE + "]: EasyHiveBed enabled"));
 	}
 
@@ -40,33 +43,41 @@ public class ModuleEasyHiveBed extends Module{
 	@Override
 	public void onTick() 
 	{
-		for(int x = radius; x >= -radius; x--)
-        {
-			for(int y = radius; y >= -radius; y--)
-            {
-            	for(int z = radius; z >= -radius; z--)
-                {
-                	BlockPos blockPos = new BlockPos(mc.thePlayer.posX + x, mc.thePlayer.posY + y, mc.thePlayer.posZ + z);
-                	BlockPos blockAbove = new BlockPos(mc.thePlayer.posX + x, mc.thePlayer.posY + y + 1, mc.thePlayer.posZ + z);
-                	
-                	if(mc.theWorld.getBlockState(blockPos).getBlock() instanceof BlockBed && mc.theWorld.getBlockState(blockAbove).getBlock().getMaterial() != Material.glass)
-                    {
-                		try
-                		{
-                			if(mc.thePlayer.inventory.getCurrentItem().getItem() instanceof ItemCloth && mc.playerController.onPlayerRightClick(mc.thePlayer, mc.theWorld, mc.thePlayer.inventory.getCurrentItem(), blockPos, EnumFacing.UP, new Vec3(0 + blockPos.getX(), -2 + blockPos.getY(), 0 + blockPos.getZ())))
-                            {
-                                mc.thePlayer.swingItem();
-                            }
-                		} 
-                		catch(Exception e)
-                 		{
-                			continue;
-                		}
-                    }
-                	
-                }
-            }
-        }
+		if(counter == 5)
+		{
+			counter = 0;
+			for(int x = radius; x >= -radius; x--)
+	        {
+				for(int y = radius; y >= -radius; y--)
+	            {
+	            	for(int z = radius; z >= -radius; z--)
+	                {
+	                	BlockPos blockPos = new BlockPos(mc.thePlayer.posX + x, mc.thePlayer.posY + y, mc.thePlayer.posZ + z);
+	                	BlockPos blockAbove = new BlockPos(mc.thePlayer.posX + x, mc.thePlayer.posY + y + 1, mc.thePlayer.posZ + z);
+	                	
+	                	if(mc.theWorld.getBlockState(blockPos).getBlock() instanceof BlockBed && mc.theWorld.getBlockState(blockAbove).getBlock().getMaterial() != Material.glass)
+	                    {
+	                		try
+	                		{
+	                			if(mc.thePlayer.inventory.getCurrentItem().getItem() instanceof ItemCloth && mc.playerController.onPlayerRightClick(mc.thePlayer, mc.theWorld, mc.thePlayer.inventory.getCurrentItem(), blockPos, EnumFacing.UP, new Vec3(0 + blockPos.getX(), -1 + blockPos.getY(), 0 + blockPos.getZ())))
+	                            {
+	                                mc.thePlayer.swingItem();
+	                            }
+	                		} 
+	                		catch(Exception e)
+	                 		{
+	                			continue;
+	                		}
+	                    }
+	                	
+	                }
+	            }
+	        }
+		}
+		else
+		{
+			counter++;
+		}
 	}
 
 	@Override
